ba345e916e2dd26964bad548e5f085b46ddbeb02
DmitryGrinchik <grinchikdmitry@gmail.com>
2024-03-17 14:25:31 +0100

    Setted fetch to main.js

--- components/screens/Main.js
+++ components/screens/Main.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
 import { styles } from '../styles/Styles';
 
 function MainScreen({navigation}) {
-  return (
-    <View style={styles.container}>
-      <Text>main</Text>
-    </View>
-  );
+
+    // 760184286672-fmu495c6mncua2oufen4gcuonrsodto1.apps.googleusercontent.com
+    // AIzaSyDXkS8b2IO05qhePzvVLZBgkfYy6MZ2Jik
+
+    const CLIENT_ID = '760184286672-fmu495c6mncua2oufen4gcuonrsodto1.apps.googleusercontent.com';
+    const API_KEY = 'AIzaSyDXkS8b2IO05qhePzvVLZBgkfYy6MZ2Jik';
+    const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
+    const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly';
+
+    let [loaded, setLoaded] = useState(false);
+    let [str, setStr] = useState('');
+
+    useEffect(()=>{
+        fetch(
+            'https://httpbin.org/get', 
+            { method: 'GET' }
+        )
+        .then( resp => resp.json() )
+        .then( data => {
+                setStr(JSON.stringify(data));
+                setLoaded(true);
+            } 
+        );
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <Text>{str}</Text>
+        </View>
+    );
 }
 
 export {MainScreen};
